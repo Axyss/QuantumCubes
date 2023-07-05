4333fe21235916942e75e47d23db6f3b116954c2
Axyss <alexnieto201@gmail.com>
2023-07-05 12:51:25 +0200

    fix: forcePlayerToOpen not working sometimes

--- src/main/java/me/axyss/quantumcubes/gui/sign/FakeSign.java
+++ src/main/java/me/axyss/quantumcubes/gui/sign/FakeSign.java
@@ -1,4 +1,4 @@
-package me.axyss.quantumcubes;
+package me.axyss.quantumcubes.gui.sign;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -8,29 +8,36 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.BlockPosition;
+import me.axyss.quantumcubes.Main;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 
-public class FakeSign {
+class FakeSign {
     private static final ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+    private static PacketAdapter textReader;
     private final Player fooledPlayer;
     private BlockData previousBlock;
     private Location currentLocation;
     private String[] text;
-    private static PacketAdapter textReader;
 
     public FakeSign(Player fooledPlayer) {
+        this.fooledPlayer = fooledPlayer;
+    }
+
+    public static void onSignUpdateDo() {
+        if (textReader != null) {
+            // Removes previous listener making sure there's only one
+            manager.removePacketListener(textReader);
+        }
         textReader = new PacketAdapter(Main.getInstance(), ListenerPriority.NORMAL, PacketType.Play.Client.UPDATE_SIGN) {
             @Override
             public void onPacketReceiving(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
-                event.setCancelled(true);
-                event.getPlayer().sendMessage(packet.getStringArrays().read(0)[0]);
             }
         };
-        this.fooledPlayer = fooledPlayer;
+        // todo finish
         manager.addPacketListener(textReader);
     }
 
@@ -53,22 +60,22 @@ public class FakeSign {
         this.currentLocation = location;
         previousBlock = fooledPlayer.getWorld().getBlockAt(getLocation()).getBlockData();
         fooledPlayer.sendBlockChange(getLocation(), Material.BIRCH_SIGN.createBlockData());
-        fooledPlayer.sendSignChange(getLocation(), new String[] {"", "Introduce a", "MC-Heads ID", ""});
+        fooledPlayer.sendSignChange(getLocation(), getText());
     }
 
-    public void dematerialize(Location location) {
+    public void dematerialize() {
         if (getLocation() == null) {
             throw new IllegalStateException("FakeSign has already been dematerialized.");
         }
-        this.currentLocation = null;
-        fooledPlayer.sendBlockChange(location, previousBlock);
+        fooledPlayer.sendBlockChange(currentLocation, previousBlock);
         previousBlock = null;
+        currentLocation = null;
         // todo esto puede dar problemillas
     }
 
     public void forcePlayerToOpen() {
         PacketContainer openSignPacket = new PacketContainer(PacketType.Play.Server.OPEN_SIGN_EDITOR);
-        openSignPacket.getBlockPositionModifier().write(0, new BlockPosition(fooledPlayer.getLocation().toVector()));
+        openSignPacket.getBlockPositionModifier().write(0, new BlockPosition(getLocation().toVector()));
         openSignPacket.getBooleans().write(0, true);
         manager.sendServerPacket(fooledPlayer, openSignPacket);
     }
