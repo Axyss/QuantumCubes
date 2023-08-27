f22c31865017571904e76e1613753f58e73080b3
Axyss <alexnieto201@gmail.com>
2023-08-27 18:29:33 +0200

    re: tab completion logic moved to GiveTabCompleter

--- src/main/java/me/axyss/quantumcubes/commands/give/GiveCommand.java
+++ src/main/java/me/axyss/quantumcubes/commands/give/GiveCommand.java
@@ -1,19 +1,14 @@
-package me.axyss.quantumcubes.commands;
+package me.axyss.quantumcubes.commands.give;
 
+import me.axyss.quantumcubes.data.QuantumCube;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
-
-public class GiveCommand implements CommandExecutor, TabCompleter {
-    private static final List<String> autocompleteOptions = List.of("give");
+public class GiveCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -31,25 +26,12 @@ public class GiveCommand implements CommandExecutor, TabCompleter {
 
         // Manages /gc give <player>
         if ((args.length == 2 || args.length == 3) && "give".equals(args[0]) && (giveToPlayer = Bukkit.getPlayer(args[1])) != null) {
-            giveToPlayer.getInventory().addItem(new ItemStack(Material.PLAYER_HEAD, giveAmount));
+            giveToPlayer.getInventory().addItem(QuantumCube.getNew(giveAmount));
             giveToPlayer.playSound(giveToPlayer, Sound.ENTITY_ITEM_PICKUP, 0.5f, 1.0f);
             return true;
         }
         return false;
     }
 
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            return null;
-        }
 
-        if (args.length == 1) {
-            return autocompleteOptions;
-        } else if (args.length == 2) {
-            return null; // Autocompletes with usernames
-        } else {
-            return List.of();
-        }
-    }
 }
