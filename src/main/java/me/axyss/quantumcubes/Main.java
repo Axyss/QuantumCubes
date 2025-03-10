2885fa5a7c4e741bd162910ed84194b5f37b29ed
Axyss <alexnieto201@gmail.com>
2025-03-10 18:13:40 +0100

    add: yml file update system

--- src/main/java/me/axyss/quantumcubes/Main.java
+++ src/main/java/me/axyss/quantumcubes/Main.java
@@ -2,6 +2,7 @@ package me.axyss.quantumcubes;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.tchristofferson.configupdater.ConfigUpdater;
 import me.axyss.quantumcubes.commands.QuantumCubesCommand;
 import me.axyss.quantumcubes.data.HeadDatabase;
 import me.axyss.quantumcubes.data.QuantumCube;
@@ -12,7 +13,10 @@ import me.axyss.quantumcubes.utils.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main extends JavaPlugin {
     private ProtocolManager protocolManager;
@@ -21,7 +25,9 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onLoad() {
+        // Required loading order: Config -> Resource update -> Language -> Others
         saveDefaultConfig();
+        updateYAMLResources("config.yml", "lang.yml");
         Language.loadLangFile(this);
         QuantumCube.setPlugin(this);
         QuantumCube.setDefaultItemValues(
@@ -56,5 +62,20 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         headDB.close();
+    }
+
+    private void updateYAMLResources(String... resourceNames) {
+        for (String resourceName: resourceNames) {
+            File resourceFile = new File(getDataFolder(), resourceName);
+            try {
+                ConfigUpdater.update(this, resourceName, resourceFile, List.of());
+            } catch (IOException e) {
+                getLogger().severe("Failed to update YAML resource: " + resourceName);
+                e.printStackTrace();
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
+        reloadConfig();
     }
 }
