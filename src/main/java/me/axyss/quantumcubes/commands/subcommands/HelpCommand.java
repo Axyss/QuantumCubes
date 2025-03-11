f0bad7bba0fdc132fdacf671d736f339dfc03c16
Axyss <alexnieto201@gmail.com>
2025-03-11 17:35:44 +0100

    fix: help text is no longer customizable if customized, it wouldn't update when a new version occurred, potentially hiding new features

--- src/main/java/me/axyss/quantumcubes/commands/subcommands/HelpCommand.java
+++ src/main/java/me/axyss/quantumcubes/commands/subcommands/HelpCommand.java
@@ -2,6 +2,7 @@ package me.axyss.quantumcubes.commands.subcommands;
 
 import me.axyss.quantumcubes.commands.SubCommand;
 import me.axyss.quantumcubes.utils.Language;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class HelpCommand implements SubCommand {
@@ -11,6 +12,12 @@ public class HelpCommand implements SubCommand {
             sender.sendMessage(Language.getMessage("invalid-syntax"));
             return;
         }
-        sender.sendMessage(Language.getMessage("help"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', """
+            &b==== Quantum Cubes Help ====
+            &9/qc help &7- Shows this help message.
+            &9/qc give <player> [amount] &7- Gives a certain amount of cubes to a player.
+            &9/qc refresh &7- Forces a manual refresh of the internal head database.
+            &9/qc reload &7- Reloads the plugin's configuration.""")
+        );
     }
 }
